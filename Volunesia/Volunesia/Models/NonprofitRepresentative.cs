53c53e07b3be568e8d80c10acb5f050fc0bb8923
Carlos Hurtado <carlos.hurtado19@yahoo.com>
2019-01-24 12:17:11 -0800

    Model Classes
    Added NonprofitRepresentative, VolunteerHistory, and Event

---
+++ Volunesia/Volunesia/Models/NonprofitRepresentative.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Volunesia.Models
+{
+    public class NonprofitRepresentative
+    {
+        // Nonprofit Representative's position title
+        public string Position    { get; set; }
+
+        // Flag to determine if representative can post an event
+        public bool   Poster      { get; set; }
+
+        // Flag to determine if representative can review an application
+        public bool   Reviewer    { get; set; }
+
+        // Flag to determine if representative can add, remove, or edit another representative and their permissions
+        public bool   RepsManager { get; set; }
+    }
+}
---
+++ Volunesia/Volunesia/Models/VolunteerHistory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Volunesia.Models
+{
+    public class VolunteerHistory
+    {
+        // List of events that the user has attended and hours they have inputted
+        public Dictionary<Event, int> VolunteeringHours { get; set; }
+    }
+}
