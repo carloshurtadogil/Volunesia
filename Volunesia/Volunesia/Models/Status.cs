7954741eb992f6f789de954f7f95df44bfba2747
Devon Wright <devon.a.wright95@gmail.com>
2019-01-25 14:39:51 -0800

    Model Classes
    Added Attendee.cs and Status.cs

---
+++ Volunesia/Volunesia/Models/Status.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Volunesia.Models
+{
+    class Status
+    {
+        public enum Statuses { WillAttend, Cancelled, NoShow, Attended }
+    }
+}
+
