81cac85d563731619c4c18132d1db62600609a8a
Muneer <darthblue27@gmail.com>
2019-02-04 17:43:53 -0800

    Added credentials checking and partial RegisterActivity traversal

--- Volunesia/Droid/VolunteerRegisterActivity.cs
+++ Volunesia/Droid/VolunteerRegisterActivity.cs
@@ -15,14 +15,43 @@ namespace Volunesia.Droid
     [Activity(Label = "VolunteerRegisterActivity")]
     public class VolunteerRegisterActivity : Activity
     {
+
+        public EditText username { get; set; }
+        public EditText emailAddress { get; set; }
+        public EditText password { get; set; }
+        public EditText confirmPassword { get; set; }
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             //Set VolunteerRegister view
             SetContentView(Resource.Layout.VolunteerRegister);
 
-            
+            username = FindViewById<EditText>(Resource.Id.usernameField);
+            emailAddress = FindViewById<EditText>(Resource.Id.emailAddressField);
+
+            password = FindViewById<EditText>(Resource.Id.passwordField);
+            confirmPassword = FindViewById<EditText>(Resource.Id.confirmPasswordField);
+
+
+            var registrationButton = FindViewById<Button>(Resource.Id.registerVolunteerButton);
+
+            registrationButton.Click += PerformVolunteerRegistration;
+
             // Create your application here
         }
+
+        public void PerformVolunteerRegistration(object sender, EventArgs e)
+        {
+            CredentialsVerification credVerification = new CredentialsVerification();
+            if(credVerification.CheckIfPasswordsMatch(password.Text, confirmPassword.Text))
+            {
+                
+            }
+        }
+
+
+        
+
+
     }
 }
