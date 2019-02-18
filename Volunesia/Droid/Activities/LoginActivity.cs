fe6ff121ebace421054b2eadfec8f6d08db41163
Devon Wright <devon.a.wright95@gmail.com>
2019-02-18 10:29:36 -0800

    Login Function

--- Volunesia/Droid/Activities/LoginActivity.cs
+++ Volunesia/Droid/Activities/LoginActivity.cs
@@ -9,16 +9,34 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Volunesia.Services;
 
 namespace Volunesia.Droid
 {
     [Activity(Label = "LoginActivity")]
     public class LoginActivity : Activity
     {
+        public EditText emailAddress { get; set; }
+        public EditText password { get; set; }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Login);
+
+            //Get email and password from user.
+            emailAddress = FindViewById<EditText>(Resource.Id.emailAddressField);
+            password = FindViewById<EditText>(Resource.Id.passwordField);
+
+            Button loginButton = FindViewById<Button>(Resource.Id.loginButton);
+
+            loginButton.Click += LoginUser;
+        }
+
+        public void LoginUser(object sender, EventArgs e)
+        {
+            AppData_Droid.GetInstance(this);
+            AppData_Droid.Auth.SignInWithEmailAndPassword(emailAddress.Text, password.Text);
         }
     }
 }
