0880948cccd30dc3a1167184e7c307a3cee3b1a4
Devon Wright <devon.a.wright95@gmail.com>
2019-02-20 10:04:59 -0800

    Login Functionality
    Login works and takes user to volunteer homepage.

--- Volunesia/Droid/Activities/LoginActivity.cs
+++ Volunesia/Droid/Activities/LoginActivity.cs
@@ -5,16 +5,18 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Gms.Tasks;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Volunesia.Droid.Activities;
 using Volunesia.Services;
 
 namespace Volunesia.Droid
 {
     [Activity(Label = "LoginActivity")]
-    public class LoginActivity : Activity
+    public class LoginActivity : Activity, IOnCompleteListener
     {
         public EditText emailAddress { get; set; }
         public EditText password { get; set; }
@@ -36,7 +38,23 @@ namespace Volunesia.Droid
         public void LoginUser(object sender, EventArgs e)
         {
             AppData_Droid.GetInstance(this);
-            AppData_Droid.Auth.SignInWithEmailAndPassword(emailAddress.Text, password.Text);
+            AppData_Droid.Auth.SignInWithEmailAndPassword(emailAddress.Text, password.Text).AddOnCompleteListener(this, this);
+        }
+
+        public void OnComplete(Task task)
+        {
+            if(task.IsSuccessful == true)
+            {
+                Console.WriteLine("Task Successful");
+                var intent = new Intent(this, typeof(VolunteerHomeActivity));
+                StartActivity(intent);
+            }
+            else
+            {
+                AlertDialog.Builder dialogAlertConstruction = new AlertDialog.Builder(this);
+                dialogAlertConstruction.SetTitle("Authentication failure");
+                dialogAlertConstruction.SetMessage("Email and/or password are incorrect.");
+            }
         }
     }
 }
