a9c7eb371ea7ecdabccffb10f913f65414b8827f
Carlos Hurtado <carlos.hurtado19@yahoo.com>
2019-04-23 09:51:38 -0700

    Images can be sent through email (iOS)

--- Volunesia/iOS/Services/EmailHandler.cs
+++ Volunesia/iOS/Services/EmailHandler.cs
@@ -18,19 +18,20 @@ namespace Volunesia.iOS.Services
             MailAddress mailAddress = new MailAddress("contactvolunesia@gmail.com", "Volunesia");
             MailAddress mailAddress1 = new MailAddress("hpfreak216@gmail.com", username);
             mail.To.Add(mailAddress1);
-            mail.From = mailAddress;
+            mail.From = mailAddress; 
             LinkedResource inline = new LinkedResource(@"VIcon.jpg", MediaTypeNames.Image.Jpeg);
             inline.ContentId = Guid.NewGuid().ToString();
             inline.TransferEncoding = TransferEncoding.Base64;
-            AlertShow.Print(inline.ToString());
-
+            AlertShow.Print(inline.ToString() + "with ID" + inline.ContentId);
             mail.Subject = "Certificate of completion";
             mail.Body = String.Format(
            "<h3>Client: " + username + " Has Sent You A Screenshot</h3>" +
            @"<img src=""cid:{0}"" />", inline.ContentId);
             mail.IsBodyHtml = true;
 
-
+            AlternateView htmlView = AlternateView.CreateAlternateViewFromString(mail.Body, null, MediaTypeNames.Text.Html);
+            htmlView.LinkedResources.Add(inline);
+            mail.AlternateViews.Add(htmlView);
 
             string sendEmailsFrom = "contactvolunesia@gmail.com";
             string sendEmailsFromPassword = "Volunesia#2019";
